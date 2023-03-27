f5cc4bde0934528cef6b55b93f354ba6aa261509
JEOSAFAELLER <101805858+JEOSAFAELLER@users.noreply.github.com>
2023-03-27 07:20:22 -0300

    Internal Class
    Implementação da classe internal

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,8 @@
 ﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
+
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 new ByteBankAtendimento().AtendimentoCliente();
+
+
 
