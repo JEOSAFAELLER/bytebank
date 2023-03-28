07ac5c711e6a6c5c92c0a1d54429867f10c301dd
JEOSAFAELLER <101805858+JEOSAFAELLER@users.noreply.github.com>
2023-03-28 19:04:29 -0300

    Configuração de DLL
    Importação de DLL

---
+++ bytebanck_GeradorChavePix/bytebanck_GeradorChavePix/GeradorPix.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace bytebanck_GeradorChavePix
+{
+    public static class GeradorPix
+    {
+        public static string GetChavePix()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static List<string> GetChavesPix(int numeroChaves)
+        {
+            if(numeroChaves <=0)
+            {
+                return null;
+                
+            }
+            else
+            {
+                var chaves = new List<string>();
+                for (int i = 0; i < numeroChaves; i++)
+                {
+                    chaves.Add(Guid.NewGuid().ToString());
+                }
+
+                return chaves;
+            }
+           
+        }
+
+    }
+}
--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,20 +1,28 @@
 ﻿using bytebank.Modelos.ADM.Funcionarios;
 using bytebank_ATENDIMENTO.bytebank.Atendimento;
 using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
+using bytebanck_GeradorChavePix;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+//new ByteBankAtendimento().AtendimentoCliente();
 
-public class Estagiario : Funcionario
-{
-    public override void AumentarSalario()
-    {
-        throw new NotImplementedException();
-    }
+Console.WriteLine(GeradorPix.GetChavePix());
 
-    protected override double getBonificacao()
-    {
-        throw new NotImplementedException();
-    }
-}
+
+//public class Estagiario : Funcionario
+//{
+//    public Estagiario(double salario, string cpf) : base(salario, cpf)
+//    {
+//    }
+
+//    public override void AumentarSalario()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    protected override double getBonificacao()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
 
